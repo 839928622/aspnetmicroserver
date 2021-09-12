473c86697a099a09312b440f8a37d9a88a04aa5b
Parker <jwk839928622@outlook.com>
2021-09-12 22:39:45 +0800

    Generate Proto Service Class from Discount proto File in Discount Grpc

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,82 @@
+﻿using System.Threading.Tasks;
+using Discount.Grpc.Protos;
+using Grpc.Core;
+using Infrastructure.Entities;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.Logging;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _repository;
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(IDiscountRepository discountRepository, ILogger<DiscountService> logger)
+        {
+            _repository = discountRepository;
+            _logger = logger;
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _repository.GetDiscount(request.ProductName);
+            if (coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName={request.ProductName} is not found."));
+            }
+            _logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount : {amount}", coupon.ProductName, coupon.Amount);
+
+            
+            return new CouponModel
+            {
+                Id = coupon.Id,
+                Amount = coupon.Amount,
+                Description = coupon.Description,
+                ProductName = coupon.ProductName
+            };
+        }
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+
+            var coupon = new Coupon()
+            {
+                Amount = request.Coupon.Amount,
+                Description = request.Coupon.Description,
+                ProductName = request.Coupon.ProductName
+            };
+
+            await _repository.CreateDiscount(coupon);
+            _logger.LogInformation("Discount is successfully created. ProductName : {ProductName}", coupon.ProductName);
+
+            return request.Coupon;
+        }
+
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = new Coupon()
+            {
+                Amount = request.Coupon.Amount,
+                Description = request.Coupon.Description,
+                ProductName = request.Coupon.ProductName
+            };
+
+            await _repository.UpdateDiscount(coupon);
+            _logger.LogInformation("Discount is successfully updated. ProductName : {ProductName}", coupon.ProductName);
+
+            return request.Coupon;
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            var deleted = await _repository.DeleteDiscount(request.ProductName);
+            var response = new DeleteDiscountResponse
+            {
+                Success = deleted
+            };
+
+            return response;
+        }
+    }
+}
