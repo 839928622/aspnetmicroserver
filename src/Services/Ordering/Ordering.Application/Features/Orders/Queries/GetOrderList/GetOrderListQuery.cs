979e86b15953c4fba2542cebc45f5983b07a9bce
Parker <jwk839928622@outlook.com>
2021-09-26 23:59:14 +0800

    CQRS Implementation with Mediator Design Pattern part!

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderListQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public  class GetOrderListQuery :  IRequest<List<OrdersDTO>>
+    {
+        public GetOrderListQuery(string userName)
+        {
+            this.UserName = userName ?? throw new  ArgumentNullException(nameof(userName));
+        }
+        public string UserName { get; set; }
+
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderListQueryHandle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrderListQueryHandle : IRequestHandler<GetOrderListQuery, List<OrdersDTO>>
+    {
+        public GetOrderListQueryHandle()
+        {
+            
+        }
+        public Task<List<OrdersDTO>> Handle(GetOrderListQuery request, 
+             CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
