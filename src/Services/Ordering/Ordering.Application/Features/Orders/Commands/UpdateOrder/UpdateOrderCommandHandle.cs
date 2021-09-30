6568e6493e057ff86e49cecb5fc3a3ef34248fd5
alex <839928622@qq.com>
2021-09-30 10:18:23 +0800

    Developing Ordering.Application Layer- Application Command Features- UpdateOrder

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Mapster;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandle : IRequestHandler<UpdateOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly Logger<UpdateOrderCommandHandle> _logger;
+
+        public UpdateOrderCommandHandle(IOrderRepository orderRepository,Logger<UpdateOrderCommandHandle> logger)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        /// <inheritdoc />
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await _orderRepository.GetByIdAsync(request.Id);
+            if (orderToUpdate == null)
+            {
+                _logger.LogError($"Order ({request.Id}) you are updating is currently not exist on database");
+                throw new ArgumentNullException(nameof(orderToUpdate));
+            }
+
+            request.Adapt(orderToUpdate);
+            await _orderRepository.UpdateAsync(orderToUpdate);
+
+            _logger.LogInformation($"Order {orderToUpdate.Id}");
+
+            return Unit.Value;
+        }
+    }
+}
