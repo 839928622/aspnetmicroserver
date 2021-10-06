b7bd7dec7d06f31c017557b016c849713368a013
Parker <jwk839928622@outlook.com>
2021-10-07 01:23:02 +0800

    Developing Ordering.Application Layer - Application Service Registrations

---
+++ src/Services/Ordering/Ordering.Application/Extensions/ApplicationServiceRegistration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviors;
+
+namespace Ordering.Application.Extensions
+{
+   public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            // common it out since I did not use it
+           // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+           
+            return services;
+        }
+    }
+}
