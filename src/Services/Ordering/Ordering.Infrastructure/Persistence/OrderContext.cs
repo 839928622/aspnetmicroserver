4445ba4686bf4fa4fc1251f9f1f151c6001a22d0
Parker <jwk839928622@outlook.com>
2021-10-07 14:49:19 +0800

    Developing Ordering.Infrastructure Layer in Clean Architecture - Persistence

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+  public  class OrderContext: DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options):base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        /// <inheritdoc />
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach(var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTimeOffset.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTimeOffset.Now;
+                        break;
+                    case EntityState.Detached:
+                        break;
+                    case EntityState.Unchanged:
+                        break;
+                    case EntityState.Deleted:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeedData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+   public class OrderContextSeedData
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeedData> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext));
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+
+            yield return new Order()
+            {
+                UserName = "Leo", FirstName = "Marsha", LastName = "Ozkaya", EmailAddress = "Leo@gmail.com",
+                AddressLine = "Bahcelievler", Country = "Turkey", TotalPrice = 350
+            };
+
+        }
+    }
+}
