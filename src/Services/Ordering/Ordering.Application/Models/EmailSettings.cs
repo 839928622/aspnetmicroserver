ad98c862cd276a4740480beece1c82f50e2bd279
Parker <jwk839928622@outlook.com>
2021-10-07 23:31:17 +0800

    Developing Ordering.Infrastructure Layer in Clean Architecture - Repositories

--- src/Services/Ordering/Ordering.Application/Models/EmailSettings.cs
+++ src/Services/Ordering/Ordering.Application/Models/EmailSettings.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ordering.Application.Models
 {
-    public  class EmailSetting
+    public  class EmailSettings
     {
         public string ApiKey { get; set; }
         public string FromAddress { get; set; }
