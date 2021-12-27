d4024a6e3a8daaeda648deefad61e19625ec2e67
Parker <jwk839928622@outlook.com>
2021-12-27 20:56:39 +0800

    shopping aggregator

--- src/ApiGateways/Shopping.Aggregator/Models/ShoppingModel.cs
+++ src/ApiGateways/Shopping.Aggregator/Models/ShoppingModel.cs
@@ -2,12 +2,7 @@
 {
     public record ShoppingModel
     {
-        public ShoppingModel(string userName, BasketModel basketWithProducts, IEnumerable<OrderResponseModel> orders)
-        {
-            UserName = userName;
-            BasketWithProducts = basketWithProducts;
-            Orders = orders;
-        }
+       
 
         public string UserName { get; init; }
         public BasketModel BasketWithProducts { get; init; }
